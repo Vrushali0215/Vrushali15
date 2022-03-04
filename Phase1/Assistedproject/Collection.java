2ccca8ad7bcea4b92ff7fc71f099ff11cf3fa8a5
Vrushali0215 <99953323+Vrushali0215@users.noreply.github.com>
2022-03-04 19:18:06 +0530

    Add files via upload

---
+++ Phase1/Assistedproject/Collection.java
@@ -0,0 +1,16 @@
+package com.mphasis.ass;
+import java.util.ArrayList;
+
+public class Collection {
+	public static void main(String[] args) {
+		ArrayList<String> animals = new ArrayList<>();
+		
+		animals.add("Dog");
+		animals.add("Cat");
+		animals.add("Horse");
+		
+		
+		System.out.println("ArrayList:" + animals);
+	}
+
+}
